ffdebd1fbda7fd2f0d86387d6dc20d125e5bac58
Samuel Gabriel Sousa <emanuel.gabriel.sousa@protonmail.com>
2021-03-28 17:16:43 -0300

    criando o recurso de adicionar um student

--- src/main/java/br/com/emanuelgabriel/microservicestudentapi/resource/StudentResourceImpl.java
+++ src/main/java/br/com/emanuelgabriel/microservicestudentapi/resource/StudentResourceImpl.java
@@ -1,5 +1,6 @@
 package br.com.emanuelgabriel.microservicestudentapi.resource;
 
+import br.com.emanuelgabriel.microservicestudentapi.model.request.StudentModelInputRequest;
 import br.com.emanuelgabriel.microservicestudentapi.model.response.StudentModelResponse;
 import br.com.emanuelgabriel.microservicestudentapi.service.StudentService;
 import lombok.AllArgsConstructor;
@@ -8,11 +9,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/students", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -29,6 +33,14 @@ public class StudentResourceImpl {
         LOGGER.info("GET /students - todos os students");
         Page<StudentModelResponse> studentResponse = this.studentService.buscarTodos(pageable);
         return studentResponse != null ? ResponseEntity.ok(studentResponse) : ResponseEntity.ok().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<StudentModelResponse> criar(@Valid @RequestBody StudentModelInputRequest request){
+        LOGGER.info("POST /students {}", request);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{idStudent}").buildAndExpand(request.getName()).toUri();
+        return ResponseEntity.created(location).body(this.studentService.criar(request));
     }
 
 }
