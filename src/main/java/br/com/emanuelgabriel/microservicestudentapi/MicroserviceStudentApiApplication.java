4d1b8b338e973f23bb1307688154b406325dbe0a
Samuel Gabriel Sousa <emanuel.gabriel.sousa@protonmail.com>
2021-03-28 13:30:45 -0300

    primeiro commit

---
+++ src/main/java/br/com/emanuelgabriel/microservicestudentapi/MicroserviceStudentApiApplication.java
@@ -0,0 +1,14 @@
+package br.com.emanuelgabriel.microservicestudentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceStudentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceStudentApiApplication.class, args);
+	}
+
+
+}
